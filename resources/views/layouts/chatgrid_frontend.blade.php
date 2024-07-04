ff3e8e3490a1488a6ff0ba6bd800b86aecf2d91e
usmanghani607 <91252603+usmanghani607@users.noreply.github.com>
2024-07-04 23:28:54 +0500

    chat grid header

--- resources/views/layouts/chatgrid_frontend.blade.php
+++ resources/views/layouts/chatgrid_frontend.blade.php
@@ -4,14 +4,20 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recz</title>
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png')  }}"> --}}
+
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/responsive.css') }}">
+    <link rel="icon" type="image/png" href="{{ secure_asset('img/favicon.png') }}">
+
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'" media='all'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png')  }}">
-    <title>Recz</title>
 
 </head>
 
