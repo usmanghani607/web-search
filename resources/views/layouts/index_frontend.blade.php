fce520ba653ee152e6c86a38a74e7f1ebdf57bf7
usmanghani607 <91252603+usmanghani607@users.noreply.github.com>
2024-07-08 11:44:06 +0500

    asset change

--- resources/views/layouts/index_frontend.blade.php
+++ resources/views/layouts/index_frontend.blade.php
@@ -4,18 +4,18 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Recz</title>
     {{-- updated --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png')  }}"> --}}
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png')  }}">
 
-    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/responsive.css') }}">
-    <link rel="icon" type="image/png" href="{{ secure_asset('img/favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ secure_asset('img/favicon.png') }}"> --}}
 
     {{-- <link rel="stylesheet" href="https://dev.therecz.com/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://dev.therecz.com/css/style.css">
