cfe380e0bdfb1909f0bf30bf3b81f1225c5e3c07
usmanghani607 <91252603+usmanghani607@users.noreply.github.com>
2024-07-13 03:38:44 +0500

    chat header

--- resources/views/layouts/search_frontend.blade.php
+++ resources/views/layouts/search_frontend.blade.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recz</title>
 
-    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png')  }}"> --}}
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png')  }}">
 
-    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/responsive.css') }}">
-    <link rel="icon" type="image/png" href="{{ secure_asset('img/favicon.png')  }}">
+    <link rel="icon" type="image/png" href="{{ secure_asset('img/favicon.png')  }}"> --}}
 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'" media='all'>
 
